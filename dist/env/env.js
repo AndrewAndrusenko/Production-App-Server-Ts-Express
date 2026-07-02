e6aa9a2ad18d2a0b60195836d3be7dbe2170bb5f
Andrey Andrusenko <aandrusenko@yandex.ru>
2026-07-02 19:36:52 +0300

    Удалена папка dist из отслеживания Git

--- dist/env/env.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ENV = void 0;
-exports.ENV = {
-    SS_NgRX_PORT: '5001'
-};
--- dist/public/apps/ssngrx/chunk-6YLM3DI2.js
+++
@@ -1,1 +0,0 @@
-var t="https://p2zpsq4w-3010.euw.devtunnels.ms/";export{t as a};
