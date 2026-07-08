f2063b07c86efed040e59fc1a9aea89fe41ca7c2
Andrey Andrusenko <aandrusenko@yandex.ru>
2026-07-08 12:46:50 +0300

    Local paths changed

--- src/env/apps-configs.ts
+++ src/env/apps-configs.ts
@@ -1,10 +1,10 @@
 import { IAppConfig } from "../types";
 
 export const APPS_CONFIG = new Map <string,IAppConfig>()
-APPS_CONFIG.set('ssngrx',{urlPath:'c:/JS/JSP/prod-server-ts-express/dist/public/apps/ssngrx/'})
-APPS_CONFIG.set('rtq',{urlPath:'c:/JS/JSP/prod-server-ts-express/dist/public/apps/rqt/'})
-APPS_CONFIG.set('aam',{urlPath:'c:/JS/JSP/prod-server-ts-express/dist/public/apps/aam/'})
-APPS_CONFIG.set('crm',{urlPath:'c:/JS/JSP/prod-server-ts-express/dist/public/apps/crm/'})
-APPS_CONFIG.set('cv_site',{urlPath:'c:/JS/JSP/prod-server-ts-express/dist/public/apps/cv-site/'})
+APPS_CONFIG.set('ssngrx',{urlPath:'c:/!Development/!Projects/prod-server-ts-express/dist/public/apps/ssngrx/'})
+APPS_CONFIG.set('rtq',{urlPath:'c:/!Development/!Projects/prod-server-ts-express/dist/public/apps/rqt/'})
+APPS_CONFIG.set('aam',{urlPath:'c:/!Development/!Projects/prod-server-ts-express/dist/public/apps/aam/'})
+APPS_CONFIG.set('crm',{urlPath:'c:/!Development/!Projects/prod-server-ts-express/dist/public/apps/crm/'})
+APPS_CONFIG.set('cv_site',{urlPath:'c:/!Development/!Projects/prod-server-ts-express/dist/public/apps/cv-site/'})
   
 
