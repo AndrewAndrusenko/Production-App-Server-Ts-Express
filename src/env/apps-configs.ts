1d2c9abbdf5cb03b88cd158810e18e0af68d1766
Andrey Andrusenko <aandrusenko@yandex.ru>
2026-07-11 07:54:03 +0300

    ruote has been fixed

--- src/env/apps-configs.ts
+++ src/env/apps-configs.ts
@@ -2,7 +2,7 @@ import { IAppConfig } from "../types";
 
 export const APPS_CONFIG = new Map <string,IAppConfig>()
 APPS_CONFIG.set('ssngrx',{urlPath:'c:/!Development/!Projects/Prod-Server-3A/dist/public/apps/ssngrx/'})
-APPS_CONFIG.set('rtq',{urlPath:'c:/!Development/!Projects/Prod-Server-3A/dist/public/apps/rqt/'})
+APPS_CONFIG.set('rtq',{urlPath:'c:/!Development/!Projects/Prod-Server-3A/dist/public/apps/rtq/'})
 APPS_CONFIG.set('aam',{urlPath:'c:/!Development/!Projects/Prod-Server-3A/dist/public/apps/aam/'})
 APPS_CONFIG.set('crm',{urlPath:'c:/!Development/!Projects/Prod-Server-3A/dist/public/apps/crm/'})
 APPS_CONFIG.set('cv_site',{urlPath:'c:/!Development/!Projects/Prod-Server-3A/dist/public/apps/cv-site/'})
